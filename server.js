3c5c6741eed8cbb783a3e4764c859665cb1cd487
reader00 <qoryamanahputra0708@gmail.com>
2023-04-05 22:25:30 +0700

    fixing database and add server status route

--- server.js
+++ server.js
@@ -1,23 +1,29 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const Article = require('./models/article')
-const articleRouter = require('./routes/articles')
-const methodOverride = require('method-override')
-const app = express()
+const express = require('express');
+const mongoose = require('mongoose');
+const Article = require('./models/article');
+const articleRouter = require('./routes/articles');
+const methodOverride = require('method-override');
+const app = express();
+require('dotenv').config();
 
-mongoose.connect('mongodb://localhost/blog', {
-  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
-})
+const PORT = process.env.PORT;
 
-app.set('view engine', 'ejs')
-app.use(express.urlencoded({ extended: false }))
-app.use(methodOverride('_method'))
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+});
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
 
 app.get('/', async (req, res) => {
-  const articles = await Article.find().sort({ createdAt: 'desc' })
-  res.render('articles/index', { articles: articles })
-})
+    const articles = await Article.find().sort({ createdAt: 'desc' });
+    res.render('articles/index', { articles: articles });
+});
 
-app.use('/articles', articleRouter)
+app.use('/articles', articleRouter);
+app.use('/status', () => 'running');
 
-app.listen(5000)
+app.listen(PORT, 'localhost', () => console.log(`Listening on port ${PORT}`));
